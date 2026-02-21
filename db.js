17f7f4c42a254493f56eda073aa084e6cdd75c3e
kixin <prathameshpohankar17@gmail.com>
2026-02-21 17:22:16 +0530

    Update database connection configuration

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 import mysql from 'mysql2/promise';
 
-
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "eligix_db",
-  password: "pratham4016h", 
+const db = mysql.createPool({
+  host: 'PASTE_YOUR_MYSQLHOST_HERE',
+  user: 'PASTE_YOUR_MYSQLUSER_HERE',
+  password: 'PASTE_YOUR_MYSQLPASSWORD_HERE',
+  database: 'PASTE_YOUR_MYSQLDATABASE_HERE',
+  port: PASTE_YOUR_MYSQLPORT_HERE, // Use the number provided, usually 3306
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-
-export default pool;
+export default db;
